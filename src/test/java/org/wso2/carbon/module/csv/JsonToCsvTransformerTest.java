8b632e83659298508f8c800639a077d116befe0b
KalinduGandara <kalindugandara16@gmail.com>
2025-04-22 23:11:35 +0530

    Add tests to check handling special characters

--- src/test/java/org/wso2/carbon/module/csv/JsonToCsvTransformerTest.java
+++ src/test/java/org/wso2/carbon/module/csv/JsonToCsvTransformerTest.java
@@ -75,7 +75,7 @@ class JsonToCsvTransformerTest {
         lenient().when(mc.lookupTemplateParameter(ParameterKey.SUPPRESS_ESCAPE_CHARACTERS)).thenReturn("false");
         when(mc.getJsonElement()).thenReturn(payloadJsonElement);
         when(mc.getJsonArrayStream()).thenReturn(payloadJsonStream);
-        when(mc.collectToCsv(new String[]{"id", "firstName", "lastName"}, false)).thenReturn(csvCollector);
+        when(mc.collectToCsv(new String[]{"id", "firstName", "lastName"}, ',', false)).thenReturn(csvCollector);
 
         ArgumentCaptor<String> payloadSetArgumentCaptor = ArgumentCaptor.forClass(String.class);
 
@@ -126,7 +126,7 @@ class JsonToCsvTransformerTest {
         lenient().when(mc.lookupTemplateParameter(ParameterKey.CUSTOM_HEADER)).thenReturn(header);
         lenient().when(mc.lookupTemplateParameter(ParameterKey.SUPPRESS_ESCAPE_CHARACTERS)).thenReturn("false");
         when(mc.getJsonArrayStream()).thenReturn(payloadJsonStream);
-        when(mc.collectToCsv(headerArray, false)).thenReturn(csvCollector);
+        when(mc.collectToCsv(headerArray, ',', false)).thenReturn(csvCollector);
 
         ArgumentCaptor<String> payloadSetArgumentCaptor = ArgumentCaptor.forClass(String.class);
 
@@ -143,5 +143,112 @@ class JsonToCsvTransformerTest {
 
         Assertions.assertEquals(expectedPayload, setPayload);
 
+    }
+    @Test
+    public void testMediate_correctlyHandlesSpecialCharacters() {
+        final String jsonPayload = "[\n" +
+                "    {\n" +
+                "        \"id\": \"1\",\n" +
+                "        \"text\": \"Normal text\",\n" +
+                "        \"with_comma\": \"Hello, world\",\n" +
+                "        \"with_quote\": \"She said \\\"Hello\\\"\",\n" +
+                "        \"with_newline\": \"Line one\\nLine two\",\n" +
+                "        \"with_all\": \"\\\"Quoted text\\\", with comma\\nand newline\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": \"2\",\n" +
+                "        \"text\": \"\",\n" +
+                "        \"with_comma\": \"Smith, John\",\n" +
+                "        \"with_quote\": \"Double quotes: \\\"\\\"\",\n" +
+                "        \"with_newline\": \"Multiple\\nNew\\nLines\",\n" +
+                "        \"with_all\": \"Comma, \\\"quote\\\" and\\nnewline combined\"\n" +
+                "    }\n" +
+                "]";
+
+        final String[] headerArray = new String[]{"id", "text", "with_comma", "with_quote", "with_newline", "with_all"};
+        final String header = "id,text,with_comma,with_quote,with_newline,with_all";
+
+        JsonParser parser = new JsonParser();
+        final JsonElement payloadJsonElement = parser.parse(jsonPayload);
+        final Stream<JsonElement> payloadJsonStream =
+                StreamSupport.stream(payloadJsonElement.getAsJsonArray().spliterator(), false);
+        final CsvCollector csvCollector = new CsvCollector(mc, headerArray, ',', false);
+
+        lenient().when(mc.lookupTemplateParameter(ParameterKey.CUSTOM_HEADER)).thenReturn(header);
+        lenient().when(mc.lookupTemplateParameter(ParameterKey.SUPPRESS_ESCAPE_CHARACTERS)).thenReturn("false");
+        when(mc.getJsonArrayStream()).thenReturn(payloadJsonStream);
+        when(mc.collectToCsv(headerArray, ',', false)).thenReturn(csvCollector);
+
+        ArgumentCaptor<String> payloadSetArgumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        JsonToCsvTransformer jsonToCsvTransformer = new JsonToCsvTransformer();
+        jsonToCsvTransformer.mediate(mc);
+
+        verify(mc).setTextPayload(payloadSetArgumentCaptor.capture());
+        String setPayload = payloadSetArgumentCaptor.getValue();
+
+        final String expectedPayload = "id,text,with_comma,with_quote,with_newline,with_all\n" +
+                "1,Normal text,\"Hello, world\",\"She said \"\"Hello\"\"\",\"Line one\n" +
+                "Line two\",\"\"\"Quoted text\"\", with comma\n" +
+                "and newline\"\n" +
+                "2,,\"Smith, John\",\"Double quotes: \"\"\"\"\",\"Multiple\n" +
+                "New\n" +
+                "Lines\",\"Comma, \"\"quote\"\" and\n" +
+                "newline combined\"\n";
+
+        Assertions.assertEquals(expectedPayload, setPayload);
+    }
+
+    @Test
+    public void testMediate_correctlyHandlesSpecialCharacters_withoutCustomHeaders() {
+        final String jsonPayload = "[\n" +
+                "    {\n" +
+                "        \"id\": \"1\",\n" +
+                "        \"text\": \"Normal text\",\n" +
+                "        \"with_comma\": \"Hello, world\",\n" +
+                "        \"with_quote\": \"She said \\\"Hello\\\"\",\n" +
+                "        \"with_newline\": \"Line one\\nLine two\",\n" +
+                "        \"with_all\": \"\\\"Quoted text\\\", with comma\\nand newline\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"id\": \"2\",\n" +
+                "        \"text\": \"\",\n" +
+                "        \"with_comma\": \"Smith, John\",\n" +
+                "        \"with_quote\": \"Double quotes: \\\"\\\"\",\n" +
+                "        \"with_newline\": \"Multiple\\nNew\\nLines\",\n" +
+                "        \"with_all\": \"Comma, \\\"quote\\\" and\\nnewline combined\"\n" +
+                "    }\n" +
+                "]";
+
+        JsonParser parser = new JsonParser();
+        final JsonElement payloadJsonElement = parser.parse(jsonPayload);
+        final Stream<JsonElement> payloadJsonStream =
+                StreamSupport.stream(payloadJsonElement.getAsJsonArray().spliterator(), false);
+        final CsvCollector csvCollector = new CsvCollector(mc, null);
+
+        lenient().when(mc.lookupTemplateParameter(ParameterKey.CUSTOM_HEADER)).thenReturn("");
+        lenient().when(mc.lookupTemplateParameter(ParameterKey.SUPPRESS_ESCAPE_CHARACTERS)).thenReturn("false");
+        when(mc.getJsonElement()).thenReturn(payloadJsonElement);
+        when(mc.getJsonArrayStream()).thenReturn(payloadJsonStream);
+        when(mc.collectToCsv(new String[]{"id", "text", "with_comma", "with_quote", "with_newline", "with_all"}
+                , ',', false)).thenReturn(csvCollector);
+
+        ArgumentCaptor<String> payloadSetArgumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        JsonToCsvTransformer jsonToCsvTransformer = new JsonToCsvTransformer();
+        jsonToCsvTransformer.mediate(mc);
+
+        verify(mc).setTextPayload(payloadSetArgumentCaptor.capture());
+        String setPayload = payloadSetArgumentCaptor.getValue();
+
+        final String expectedPayload = "1,Normal text,\"Hello, world\",\"She said \"\"Hello\"\"\",\"Line one\n" +
+                "Line two\",\"\"\"Quoted text\"\", with comma\n" +
+                "and newline\"\n" +
+                "2,,\"Smith, John\",\"Double quotes: \"\"\"\"\",\"Multiple\n" +
+                "New\n" +
+                "Lines\",\"Comma, \"\"quote\"\" and\n" +
+                "newline combined\"\n";
+
+        Assertions.assertEquals(expectedPayload, setPayload);
     }
 }
